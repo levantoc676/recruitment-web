ac3bc078fe14dcb7d73f816ebb1c81ea1f86d309
WD <levantoc676@gmail.com>
2025-09-27 15:27:46 +0700

    WD add database

---
+++ src/main/java/com/web/hr/recruitment/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.web.hr.recruitment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**", "/css/**", "/js/**", "/images/**").permitAll() // cho phép truy cập login, static files
+                        .anyRequest().authenticated() // các request khác phải login
+                )
+                .formLogin(form -> form
+                        .loginPage("/auth/login")    // dùng login.html của bạn
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+}
