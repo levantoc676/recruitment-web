f566dd62b9ee4d15369056583781079fa74a2527
WD <levantoc676@gmail.com>
2025-09-27 13:47:02 +0700

    WD config web and run login

---
+++ src/main/java/com/web/hr/recruitment/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.web.hr.recruitment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    // URL: http://localhost:8080/auth/login
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+        // -> Spring Boot sẽ tìm file templates/auth/login.html
+    }
+
+    // URL: http://localhost:8080/auth/register
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
+        // -> Spring Boot sẽ tìm file templates/auth/register.html
+    }
+}
